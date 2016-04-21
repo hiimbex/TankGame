371cc2afe4460bcb7f096028f78804f352e75c3e
Bex Warner <hiimbex@stuarts-mbp-3.american.edu>
2016-04-21 18:27:32 -0400

    the ui script struggle is real

--- Tank Game/Assets/Scripts/GameManagerScript.cs
+++ Tank Game/Assets/Scripts/GameManagerScript.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
+using UnityEngine.UI;
+
 public class GameManagerScript : MonoBehaviour {
 
 	public static List<EnemyTurretScript> enemyTurretScriptList = new List<EnemyTurretScript>();
@@ -9,15 +11,19 @@ public class GameManagerScript : MonoBehaviour {
 	//This works for now but it would be better to just get the length of the enemyTurretScriptList.
 	public static int enemyCount;
 	public static int scoreCountEnemy;
+	//public Text turretsDestroyed;
 	// Use this for initialization
+
 	void Start () {
+		turretsDestroyed = gameObject.GetComponent<Text>();
 		//Cursor.visible = false;
 		//Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//scoreCountEnemy = scoreCountEnemy.ToString ();
+		//turretsDestroyed.text = "Turrets Destroyed: " + scoreCountEnemy;
 	}
 
 	public static void ReduceEnemyCount (EnemyTurretScript turret) {
@@ -25,8 +31,9 @@ public class GameManagerScript : MonoBehaviour {
 		scoreCountEnemy++;
 		enemyCount -= 1;
 		Debug.Log (enemyCount);
-
+		//turretsDestroyed.text = "Turrets Destroyed: " + scoreCountEnemy;
 	}
+
 	void OnGUI() {
 		GUI.Label (new Rect(20, 40, 400, 400), "Turrets Destroyed: " + scoreCountEnemy);
 	}
