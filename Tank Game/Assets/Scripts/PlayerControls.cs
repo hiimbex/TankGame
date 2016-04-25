d649a606a8c71a0eaadc8480b8b9e645e35833b5
Bex Warner <hiimbex@stuarts-mbp-3.american.edu>
2016-04-24 22:59:33 -0400

    ui changes

--- Tank Game/Assets/Scripts/PlayerControls.cs
+++ Tank Game/Assets/Scripts/PlayerControls.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerControls : MonoBehaviour {
 
@@ -12,16 +13,16 @@ public class PlayerControls : MonoBehaviour {
 	float moveSpeed = 3f;
 	float yVel = 0;
 	float gravity = -0.5f;
-	int score = 0;
+	//	int score = 0;
 	public static int ammo = 10;
+	public Text ammoLeft;
 
 
 	void Start () {
 		cc = gameObject.GetComponent<CharacterController> ();
 		GameManagerScript.player = this.gameObject; 
-
 	}
-		
+
 	void Update () {
 
 		//generic move code from mike
@@ -47,6 +48,8 @@ public class PlayerControls : MonoBehaviour {
 		cc.Move (amountToMove);
 		transform.Rotate (amountToRotate);
 
+		ammoLeft.text = "Ammo: " + ammo; //updates the ammo through the ui
+
 		if (Input.GetMouseButtonDown (0) && ammo > 0) {
 			GameObject tankShellClone = (GameObject)GameObject.Instantiate (tankShell, tankShellSpawn.position, tankShellSpawn.rotation );
 			tankShellClone.GetComponent<Rigidbody> ().AddForce (tankShellSpawn.forward * shootSpeed); 
@@ -62,16 +65,20 @@ public class PlayerControls : MonoBehaviour {
 			Debug.Log ("On the road");
 			moveSpeed = moveSpeed + 3f;
 		} 
+
 		if (other.gameObject.tag == "Ammo") {
 			other.gameObject.SetActive (false);
 			ammo = ammo + 10;
-			Debug.Log ("GOT AMMO!");
+			Debug.Log ("GOT AMMO!"); 
+
+
+
 			//moveSpeed = moveSpeed - 2f;
-//		} else if (other.gameObject.CompareTag ("Pick Up")) {
-//			//collecting objects so they disappear after collected and add them to the score
-//			other.gameObject.SetActive (false);
-//			moveSpeed = moveSpeed + 3f;
-//			score++;
+			//		} else if (other.gameObject.CompareTag ("Pick Up")) {
+			//			//collecting objects so they disappear after collected and add them to the score
+			//			other.gameObject.SetActive (false);
+			//			moveSpeed = moveSpeed + 3f;
+			//			score++;
 		} else if (other.gameObject.CompareTag ("Shell")) {
 			//Debug.Log ("Player has been hit!");
 		}
@@ -82,10 +89,11 @@ public class PlayerControls : MonoBehaviour {
 			moveSpeed = moveSpeed - 3f;
 		}
 	}
-	void OnGUI() {
-		//displays the score
-		//sGUI.Label (new Rect(20, 20, 200, 200), "POWERUPS: " + score);
-		GUI.Label (new Rect(20, 20, 400, 400), "Speed: " + moveSpeed + "0mph");
-		GUI.Label (new Rect(20, 60, 400, 400), "Ammo: " + ammo);
-	}
+
+	//	void OnGUI() {
+	//		//displays the score
+	//		//sGUI.Label (new Rect(20, 20, 200, 200), "POWERUPS: " + score);
+	//		GUI.Label (new Rect(20, 20, 400, 400), "Speed: " + moveSpeed + "0mph");
+	//		GUI.Label (new Rect(20, 60, 400, 400), "Ammo: " + ammo);
+	//	}
 }
