5dea34baf4bb6f9ce7be6f5a6a2ff567638e5483
Bex Warner <rw1048a@student.american.edu>
2016-05-01 16:43:15 -0400

    button added not yet working

---
+++ Tank Game/Assets/testscript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class testscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("testing for scene change start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void clickTest () {
+		Debug.Log ("testing for scene change function");
+		SceneManager.LoadScene ("GameLostScene");	
+	
+	}
+}
