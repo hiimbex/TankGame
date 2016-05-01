1c7c7acd2976eceae1d267579f5a075b7c381b24
ASandhuZero <as7687a@american.edu>
2016-05-01 17:06:15 -0400

    Fixed button issue

---
+++ Tank Game/Assets/Scripts/TestButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TestButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void onClick () {
+		Debug.Log ("testing for scene change");
+		SceneManager.LoadScene ("GameLostScene");	
+	}
+}
